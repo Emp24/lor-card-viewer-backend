fcb5bf6ff2eeb06cd0f7255597b0c0503a9f869d
Emp <40623900+Emp24@users.noreply.github.com>
2022-11-25 00:16:39 +0200

    data

---
+++ player&match_info_json.js
@@ -0,0 +1,76 @@
+// Get summoner by Name header
+{
+    "id": "JC4vyKwcLhWK-yWM2mwtuvI-dzVzQ8wLxVCmHphsqXBHqT4",
+    "accountId": "-A79DzUVtXFLu0AiJ-3Fdyhj7FxvBttb3kMGO6d0V79riBg",
+    "puuid": "GUg_0LUKVGQgGdkmBr2j9YDJBcqSSiNaAGuHXXz3yH1KaOu5v-qK69ueEwTxBUlCACsqPFcwDUo2mQ",
+    "name": "Empti",
+    "profileIconId": 4414,
+    "revisionDate": 1614007144000,
+    "summonerLevel": 188
+}
+//List of matches
+[
+    "dccc20db-5a93-4812-8e9e-564e7eda45e8",
+    "fd61930d-5e93-451b-ab3e-9408dd4df729",
+    "f936b39b-396d-4fda-b693-24e5338c83a1",
+    "71ea82f7-a89e-47d7-a0c3-6bc0bf1aadff",
+    "886c73ed-3f2c-4d94-a3ca-669cc9ddd813",
+    "c2ab495f-e2fa-4546-961c-2870fe07e223",
+    "99a65048-ff9d-4bcd-ab85-6eae2f7ce659",
+    "3f115143-5f0c-4270-bc95-be4909283fdb",
+    "37fd6697-b9f4-45d5-8fbc-4fb01d0c0468",
+    "6e21c281-82fa-40b1-9685-d7f1d632c727",
+    "40a0f3dd-736e-4072-a7cd-2ad3a85ba5b6",
+    "7f215405-64b3-4c92-b5f8-ddfbe2584b99",
+    "2d74bde9-36cd-4890-a6f3-9b9be0d8835f",
+    "2e74ed77-8b56-4ff0-b8dd-1b5ed6ff16ad",
+    "d332673c-296f-4553-8f7c-1a376f2f4832",
+    "83012222-e976-4320-b902-d10413bae712",
+    "01ed4a39-aba4-458f-b2cd-de317f701c97",
+    "d0ebb725-92e6-48c1-9180-e119f8cf3a17",
+    "25480bde-05ad-4d5c-81e0-5cbb660d448e",
+    "8c0af8c6-b437-4bca-b675-7d5390d13256"
+]
+
+//Response of get match by ID
+{
+    "metadata": {
+        "data_version": "2",
+        "match_id": "dccc20db-5a93-4812-8e9e-564e7eda45e8",
+        "participants": [
+            "mypHMpbh7bVZAj8vtq17MQ9N_OgNoqm0TENCv7VEgmMSk-kMNvNkVdkpkLfNHuZs6xlVsQROyzKIqg",
+            "GUg_0LUKVGQgGdkmBr2j9YDJBcqSSiNaAGuHXXz3yH1KaOu5v-qK69ueEwTxBUlCACsqPFcwDUo2mQ"
+        ]
+    },
+    "info": {
+        "game_mode": "Expeditions",
+        "game_type": "Normal",
+        "game_start_time_utc": "2021-02-26T10:38:17.7886000+00:00",
+        "game_version": "live_2_2_14",
+        "players": [
+            {
+                "puuid": "mypHMpbh7bVZAj8vtq17MQ9N_OgNoqm0TENCv7VEgmMSk-kMNvNkVdkpkLfNHuZs6xlVsQROyzKIqg",
+                "deck_id": "276e129c-fb2d-58d0-a698-7aee2347c637liveLimitedDeck",
+                "deck_code": "CMAAKAIBAI3QCAQAAEAQEAQBAEBQECACAEABULIEAIAQEERLAIBAEBAIAMBQEAICBEDACAAGBIGRYIJFAQAQACIEAEACA",
+                "factions": [
+                    "faction_Demacia_Name",
+                    "faction_Ionia_Name"
+                ],
+                "game_outcome": "loss",
+                "order_of_play": 0
+            },
+            {
+                "puuid": "GUg_0LUKVGQgGdkmBr2j9YDJBcqSSiNaAGuHXXz3yH1KaOu5v-qK69ueEwTxBUlCACsqPFcwDUo2mQ",
+                "deck_id": "0992d7ad-457e-5c10-9e41-a3312d418276liveLimitedDeck",
+                "deck_code": "CMAAGAIDBERQEAIFDI3AEAYFAMCAGBIBAUBQKDQQCIDAGBICAUDAQCQNBQBQSGREGM3DSQ2HJBEV4ZGZAE",
+                "factions": [
+                    "faction_MtTargon_Name",
+                    "faction_ShadowIsles_Name"
+                ],
+                "game_outcome": "win",
+                "order_of_play": 1
+            }
+        ],
+        "total_turn_count": 1
+    }
+}
